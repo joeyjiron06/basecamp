ee43e24cc25472bc6109f2ac9803376cb7de684f
joeyjiron06 <joeyjiron06@gmail.com>
2024-03-14 23:36:02 -0700

    added site config to astro for links to work properly

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://github.com/joeyjiron06/grizzly-css',
   integrations: [
     tailwind({ nesting: true }),
     mdx({
