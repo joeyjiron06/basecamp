3b4d8621b8837423c0f3d8719e4d02ce9f9f3d5d
joeyjiron06 <joeyjiron06@gmail.com>
2024-03-15 16:55:15 -0700

    added dark theme to website

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,29 @@
+import { createThemes } from 'tw-colors';
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: ['variant', [
+		'@media (prefers-color-scheme: dark) { &:not(.light *) }',
+		'&:is(.dark *)',
+	]],
 	theme: {
 		extend: {
 			maxWidth: {
 				'page': '72rem',
 			},
-			colors: {
+		}
+	},
+	plugins: [
+		createThemes({
+			light: {
 				// for primary buttons, links etc
 				"primary": "black",
 				"primary-fg": "white", // foreground
 
 				// secondary accent colors
 				"secondary": "grey",
-				"primary-fg": "white", // foreground
-
+				"secondary-fg": "white", // foreground
 
 				// backgrounds
 				"base": "white",
@@ -26,8 +34,31 @@ export default {
 				"fg": "black",
 				"fg-100": "#373737",
 				"fg-200": "#707070",
+			},
+			dark: {
+				// for primary buttons, links etc
+				"primary": "white",
+				"primary-fg": "black", // foreground
+
+				// secondary accent colors
+				"secondary": "white",
+				"secondary-fg": "grey", // foreground
+
+				// backgrounds
+				"base": "black",
+				"base-100": "#373737",
+				"base-200": "#707070",
+
+				// foregrounds
+				"fg": "white",
+				"fg-100": "#F0F0F0",
+				"fg-200": "#E0E0E0",
 			}
-		}
-	},
-	plugins: [],
+		}, {
+			defaultTheme: {
+				light: 'light',
+				dark: 'dark'
+			}
+		})
+	],
 }
